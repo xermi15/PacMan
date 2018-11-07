cb62959df37ae5c32439c11c4617fa0a15506de3
xermi15 <xavi.franchtarazaga@gmail.com>
2018-11-07 19:21:10 +0100

    v0.2.1

--- public_html/script.js
+++ public_html/script.js
@@ -104,35 +104,47 @@ function pintar() {
 //NOT DONE
 //- Que no estigui en la mateixa posicio que un fantasma, ni a prop!
 //- Direccio inicial a la que vol anar?
+//que despres de moures el tauler marqui una altre cop buit(1)
 function dadesInicialsJugador(jugador) {
-    var x, y, dir1, dir2;
+    var x, y;
 
     do {
         x = Math.floor((Math.random() * 29) + 0);
         y = Math.floor((Math.random() * 42) + 0);
     } while (!comprobarPosicio(y, x))
 
-    dir1 = direccioInicial(y, x);
-    dir2 = dir1;
+
+    //donem valors al jugador
+    jugador[1] = x;
+    jugador[2] = y;
+    jugador[3] = direccioInicial(y, x);
+    jugador[4] = direccioInicial(y, x);
     
     //introduim el jugador al tauler
     tauler[y][x] = 2;
+
 }
 
 //DONE
 //- assigna dades inicials a l'element fantasma
 //NOT DONE
-//-
+//-separats
+//que despres de moures el tauler marqui una altre cop buit(1)
 function dadesInicialsFantasma(fantasma) {
-    var x, y, dir1;
+    var x, y;
+    var separats = false;
 
     do {
         x = Math.floor((Math.random() * 29) + 0);
         y = Math.floor((Math.random() * 42) + 0);
     } while (!comprobarPosicio(y, x))
 
-    dir1 = direccioInicial(y, x);
-    //introduim el jugador al tauler
+    //donem valors al fantasma
+    fantasma[1] = x;
+    fantasma[2] = y;
+    fantasma[3] = direccioInicial(y, x);
+
+    //introduim el fantasma al tauler
     tauler[y][x] = 3;
 }
 
@@ -149,14 +161,14 @@ function comprobarPosicio(Y, X) {
 }
 
 //DONE
-//assigna una nova direccio aleatoria a un element
+//assigna una direccio inicial aleatoria a un element
 //NOT DONE
 //-
 function direccioInicial(Y, X) {
     var direccions = new Array();
     var trobada = false;
     var dirIni;
-    var x;
+    var aux;
     
     //comprovem si podem anar a cada una de les direccions
     if(comprobarPosicio(Y+1, X) == 1) direccions[1] = 1;
@@ -170,10 +182,10 @@ function direccioInicial(Y, X) {
 
     //Assignem una direccio aleatoria de les disponibles, a una var auxiliar
     do {
-        x = Math.floor((Math.random() * 4) + 1);
-        if(direccions[x] == 1) {
+        aux = Math.floor((Math.random() * 4) + 1);
+        if(direccions[aux] == 1) {
             trobada = true;
-            dirIni = x;
+            dirIni = aux;
         }
     } while (!trobada)
 
@@ -185,31 +197,41 @@ function direccioInicial(Y, X) {
 //assigna una nova direccio aleatoria a un element
 //NOT DONE
 //- Que la direccio no sigui aleatoria
-function novaDireccio(Y, X) {
+function novaDireccioFantasma(fantasma) {
     var direccions = new Array();
     var trobada = false;
     var novaDir;
-    var x;
+    var aux;
 
     //comprovem si podem anar a cada una de les direccions
-    if(comprobarPosicio(Y+1, X) == 1) direccions[1] = 1;
+    if(comprobarPosicio(fantasma[2]+1, fantasma[1]) == 1) direccions[1] = 1;
     else direccions[1] = 0;
-    if(comprobarPosicio(Y, X+1) == 1) direccions[2] = 1;
+    if(comprobarPosicio(fantasma[2], fantasma[1]+1) == 1) direccions[2] = 1;
     else direccions[2] = 0;
-    if(comprobarPosicio(Y-1, X) == 1) direccions[3] = 1;
+    if(comprobarPosicio(fantasma[2]-1, fantasma[1]) == 1) direccions[3] = 1;
     else direccions[3] = 0;
-    if(comprobarPosicio(Y, X-1) == 1) direccions[4] = 1;
+    if(comprobarPosicio(fantasma[2], fantasma[1]-1) == 1) direccions[4] = 1;
     else direccions[4] = 0;
 
     //Assignem una direccio aleatoria de les disponibles, a una var auxiliar
     do {
-        x = Math.floor((Math.random() * 4) + 1);
-        if(direccions[x] == 1) {
+        aux = Math.floor((Math.random() * 4) + 1);
+        if(direccions[aux] == 1) {
             trobada = true;
-            novaDir = x;
+            novaDir = aux;
         }
     } while (!trobada)
 
-    //retornem la direccio inicial
-    return novaDir;
+    //assignem la nova direccio al fantasma
+    fantasma[3] = novaDir;
 }
+
+function moureFantasma(fantasma){
+
+}
+
+
+//set time out
+//cada cert temps executa una accio, o varies
+//Que fara la funcio: donarlis la nova posicio i calcular la nova direccio (nomes cruilla o paret)
+//Extra: probabilitat de la nova direccio
